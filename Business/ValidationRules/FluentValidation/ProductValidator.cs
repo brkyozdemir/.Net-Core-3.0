2d80d35a955a4af0464abd8810d3b6aaa228750c
berkay özdemir <berkayyy18191607@hotmail.com>
2020-05-23 21:24:33 +0300

    Final commit

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Constants;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Ürün ismi gereklidir.");
+            RuleFor(p => p.ProductName).Length(2, 30);
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(1);
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p=>p.CategoryId==1);
+            RuleFor(p => p.ProductName).Must(StartWithA);
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using ValidationException = System.ComponentModel.DataAnnotations.ValidationException;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var result = validator.Validate(entity);
+            if (!result.IsValid) throw new ValidationException(result.Errors.ToString());
+        }
+    }
+}
